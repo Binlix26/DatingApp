112e19ddcf5236eb8a916873ca33f6b47414e557
Bin Li <lb583119533@outlook.com>
2019-05-02 15:27:09 +1200

    add delete in spa

--- DatingApp-SPA/src/app/messages/messages.component.ts
+++ DatingApp-SPA/src/app/messages/messages.component.ts
@@ -50,4 +50,19 @@ export class MessagesComponent implements OnInit {
     this.loadMessages();
   }
 
+  deleteMessage(id: number, event: any) {
+    event.stopPropagation();
+    this.alertify.confirm(
+      'Are you sure you want to delete this message',
+      () => {
+        this.userService.deleteMessage(id, this.authService.decodedToken.nameid)
+          .subscribe(() => {
+            this.messages.splice(this.messages.findIndex(m => m.id === id), 1);
+            this.alertify.success('Message has been deleted');
+          }, error => {
+            this.alertify.error(error);
+          });
+      });
+  }
+
 }
